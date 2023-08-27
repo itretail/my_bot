d1d3c37a9e410b705d5d1cff17d1ca7760ed7a34
itretail <itretail@hotmail.com>
2023-08-27 12:49:13 +0100

    ros control sim working

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -41,7 +41,7 @@ def generate_launch_description():
             executable='teleop_node',
             name='teleop_node',
             parameters=[joy_params],
-#            remappings=[('/cmd_vel','/cmd_vel_joy')]
+            remappings=[('/cmd_vel','/diff_cont/cmd_vel_unstamped')]
          )
     
     # Include the Gazebo launch file, provided by the gazebo_ros package
